10f37f692875faad7927c7638ae2e7a224e81b45
wu <wu@github.com>
2021-04-20 20:30:12 +0800

    [gee] context

--- Gee/context/gee/context.go
+++ Gee/context/gee/context.go
@@ -1,9 +1,18 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type H map[string]interface{}
 
 type Context struct {
 	Writer     http.ResponseWriter
 	Req        *http.Request
 	Path       string
+	Method     string
 	StatusCode int
 }
 
@@ -26,20 +35,20 @@ func (c *Context) Query(key string) string {
 
 func (c *Context) Status(code int) {
 	c.StatusCode = code
-	c.Writer.WriterHeader(code)
+	c.Writer.WriteHeader(code)
 }
 
-func (c *Context) SetHeader(key string, value string){
+func (c *Context) SetHeader(key string, value string) {
 	c.Writer.Header().Set(key, value)
 }
 
-func (c *Context) String(code int, format string, values ...interface{}){
+func (c *Context) String(code int, format string, values ...interface{}) {
 	c.SetHeader("Content-Type", "text/plain")
 	c.Status(code)
 	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
 }
 
-func (c *Context) JSON(code int, obj interface()){
+func (c *Context) JSON(code int, obj interface{}) {
 	c.SetHeader("Content-Type", "text/plain")
 	c.Status(code)
 	encoder := json.NewEncoder(c.Writer)
@@ -48,3 +57,13 @@ func (c *Context) JSON(code int, obj interface()){
 	}
 }
 
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
