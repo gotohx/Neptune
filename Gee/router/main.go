7e13d824b31cb9614e02e1cdb9da3388452accbd
wu <wu@github.com>
2021-04-21 20:28:27 +0800

    [gee] router

---
+++ Gee/router/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "Hello Gee")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+	r.Run(":9999")
+}
